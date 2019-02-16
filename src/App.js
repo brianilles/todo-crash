63128489280939a74ae7e08176885f245519fe6c
Brian Illes <brianrilles@gmail.com>
2019-02-16 13:29:55 -0800

    added basic CRUD functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   state = {
@@ -22,12 +24,51 @@ class App extends Component {
         completed: false
       }
     ]
-  }
+  };
+
+  // Toggle Complete
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  // Delete Todo
+  delTodo = id => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  };
+
+  // Add Todo
+  addTodo = title => {
+    const newTodo = {
+      id: Date.now(),
+      title,
+      completed: false
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  };
 
   render() {
     return (
       <div className="App">
-      <Todos todos={this.state.todos} />
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo} />
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            delTodo={this.delTodo}
+          />
+        </div>
       </div>
     );
   }
